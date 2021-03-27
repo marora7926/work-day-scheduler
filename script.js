e5598ab4a77a6b5b4acc477e5e9dfe5633b2ed31
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-27 16:20:02 +1100

    timer working and updating

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// reference to important DOM custom elements
+var timeDisplayEl = $('#currentDay');
+
 // adding the current day, date and time in the jubotron
-var liveDDT = moment().format('LLLL');
-$("#currentDay").text(liveDDT);
+function displayTime (){
+    var liveDDT = moment().format('MMMM Do YYYY, h:mm:ss a');
+    timeDisplayEl.text(liveDDT);
+}
+
+// updating seconds every second.
+setInterval(displayTime, 1000);
