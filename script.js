d57fafd3b0981d383d7abfd40645caf69e90047d
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-27 17:57:43 +1100

    psuedo code

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ var timeDisplayEl = $('#currentDay');
 
 // adding the current day, date and time in the jubotron
 function displayTime (){
-    var liveDDT = moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDisplayEl.text(liveDDT);
+    var liveDay = moment().format('dddd');
+    var liveDT = moment().format('MMMM Do YYYY, h:mm:ss a');
+    timeDisplayEl.text(liveDay + ", " + liveDT);
 }
 
-// updating seconds every second.
+// updating seconds every second
 setInterval(displayTime, 1000);
+
+// developing layout of the calender
+
+
+// time-block and hour content
+
+
+// form with textarea
+
+
+// save botton with save icon and prompting user to save the text
+// saving data from textarea to local storage after pressing the save button.
+
+
+// repeat the same for X number of times 9am to 5pm
+
+
+// as the day progress, the textarea changes the color usinf pasy, present and future css style
+
