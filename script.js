4b5e6c5aa7a8922f141f6562e52718d2af184dba
Mohit Arora <mohit.arora@sydney.edu.au>
2021-04-02 00:40:25 +1100

    fixed a few bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// LIVE DATE AND TIME DISPLAY ELEMENT
 // reference to important DOM custom elements
 var timeDisplayEl = $('#currentDay');
-// var hour1 = parseInt("hour": 0);
 
 // adding the current day, date and time in the jubotron
 function displayTime (){
@@ -12,53 +12,66 @@ function displayTime (){
 // updating seconds every second
 setInterval(displayTime, 1000);
 
-// $(document).ready(function(){
-// });
-
-// time-block and hour content
-// this will extract integer value of hour
-var timeCurrent = moment().hour();
-console.log(timeCurrent);
-
-// this will extract the integer value of timeblockTime
-var timeblockTime = parseInt($(this).attr("id"));
-console.log(timeblockTime);
-
-// loops as the day progress, the textarea changes the color using past, present and future css style
+// MAIN DISPLAY ELEMENT
 function timeBlock() {
-    // var timeblockTime = parseInt($("hour"));
+
+    // this will extract integer value of curent hour
+    var timeCurrent = moment().hour();
+    console.log(timeCurrent);
+    
+     // loops as the day progress, the textarea changes the color using past, present and future css style
+    $(".time-block").each(function (){    
+     // this will extract the integer value of timeblockTime
+    var timeblockTime = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(timeblockTime);
+
         if  (timeblockTime === timeCurrent){
-            timeBlock.removeClass("past")
-            timeBlock.addClass("present")
+            $(this).removeClass("past")
+            $(this).addClass("present")
         } else {
-            timeBlock.removeClass("past")
-            timeBlock.removeClass("present")
-            timeBlock.addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
         }
-}; 
-
-// Shadow pulse off to start with
-var shadowPulseOff = $(".fas").removeClass("shadowPulse");
-
-// save text botton with save icon
-$(".saveBtn").on("click",function(){
-    var userText = $(this).siblings(".description").val();
-    var userTime = $(this).parent().attr("id");
-    localStorage.setItem(userText, userTime);
-})
-
-// shadow pulse on after after entering the data to prompt user to save
-var shadowPulseOn = $(".fas").addClass("shadowPulse");
-
-// get item from local storage to save in the textarea after presisng the save button
-function getTimeAndText(){
-    $("#Time9Am .description").val(localStorage.getItem("Time9Am"));
-    $("#Time10am .description").val(localStorage.getItem("Time10Am"));
-    $("#Time11Am .description").val(localStorage.getItem("Time11Am"));
-    $("#Time12Pm .description").val(localStorage.getItem("Time12Pm"));
-    $("#Time1Pm .description").val(localStorage.getItem("Time1Pm"));
-    $("#Time2Pm .description").val(localStorage.getItem("Time2Pm"));
-    $("#Time3Pm .description").val(localStorage.getItem("Time3Pm"));
-    $("#Time4Pm .description").val(localStorage.getItem("Time4Pm"));
-    $("#Time5Pm .description").val(localStorage.getItem("Time5Pm"));
+    })
 };
+
+$(document).ready(function () {
+    timeBlock();
+
+    function shadowPulse () {
+        
+        // Shadow pulse variable for turning it off to start with
+        var shadowPulseOff = $(".fas").removeClass("shadowPulse");
+        // shadow pulse variable for turning it on after entering the data to prompt user to save
+        var shadowPulseOn = $(".fas").addClass("shadowPulse");
+        
+        if ("<textarea>" === ""){
+            shadowPulseOff = true;
+        } else {
+            shadowPulseOn = true;
+        };
+    }
+    
+    shadowPulse();
+
+    // save text botton with save icon
+    $(".saveBtn").on("click",function(){
+        var userText = $(this).siblings(".description").val();
+        var userTime = $(this).parent().attr("id");
+        localStorage.setItem(userText, userTime);
+    })
+
+    // get item from local storage to save in the textarea after presisng the save button
+    function getTimeAndText(){
+        $("#time9Am .description").val(localStorage.getItem("time9Am"));
+        $("#time10am .description").val(localStorage.getItem("time10Am"));
+        $("#time11Am .description").val(localStorage.getItem("time11Am"));
+        $("#time12Pm .description").val(localStorage.getItem("time12Pm"));
+        $("#time1Pm .description").val(localStorage.getItem("time1Pm"));
+        $("#time2Pm .description").val(localStorage.getItem("time2Pm"));
+        $("#time3Pm .description").val(localStorage.getItem("time3Pm"));
+        $("#time4Pm .description").val(localStorage.getItem("time4Pm"));
+        $("#time5Pm .description").val(localStorage.getItem("time5Pm"));
+    };
+})
